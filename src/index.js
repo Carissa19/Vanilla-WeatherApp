adb453644ac0ec0687867642130329fedb4bd718
Carissa19 <137693399+Carissa19@users.noreply.github.com>
2023-08-07 15:41:15 +0200

    Added CSS background

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ function displayforecast(response) {
     `
     <div class="col-2">
                  <div class="card-title">${formatDay(forecastDay.dt)}</div>
-                  <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" width="36" />
+                  <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" width="76" />
                   <div class="weather-forecast-temperatures">
                   <span class="weather-forecast-temperature-max">${Math.round(forecastDay.temp.max)}° </span>
                   <span class="weather-forecast-temperature-min">${Math.round(forecastDay.temp.min)}° </span>
@@ -57,7 +57,7 @@ function displayWeatherCondition(response) {
     );
     iconElement.setAttribute(
     "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png `
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 
